e84f4cfb934ac437deb2f21660b04bcfdca978a2
Qbatch <qbatch@Qbatchs-MacBook-Pro.local>
2022-11-23 14:07:06 +0500

    Frontend: Change in footer

--- frontend/src/components/footer/footer.js
+++ frontend/src/components/footer/footer.js
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <div>
       <div className='footer container-fluid d-flex justify-content-center align-items-center bg-black text-white mt-2'>
-        <h6 className='d-flex align-items-center'>Copyright 2022 - All Rights Reserved</h6>
+        <h6 className='d-flex align-items-center'>Copyright 2022 - All Rights Reserved.</h6>
       </div>
     </div>
   )
