42c9c57a689d5dc8ba85d72bc8c41f99e41b720f
Qbatch <qbatch@Qbatchs-MacBook-Pro.local>
2022-11-23 18:13:31 +0500

    add new file

---
+++ frontend/src/components/footer/test.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./footer.css"
+
+function Footer() {
+  return (
+    <div>
+      <>New File</>
+      <>New File</>
+      <>New File</>
+      <>New File</>
+      <>New File</>
+      <>New File</>
+      <>New File</>
+    </div>
+  )
+}
+
+export default Footer
